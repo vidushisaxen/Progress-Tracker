cf36b5fc58951a9dfdab6560b25a223f67b46cf9
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-11-24 14:46:08 +0530

    main

--- script.js
+++ script.js
@@ -8,12 +8,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   
     inputElement.addEventListener("input", function () {
-       
         let inputValue = inputElement.value;
+       if(inputValue>=0 && inputValue<=100){
         textElement.textContent = inputValue + "%";
         let dashArray = (inputValue / 100) * 1069;
         circleElement.setAttribute("stroke-dasharray", dashArray + " 1069");
+       }else{
+        inputElement.value=" ";
+        alert("Please enter a valid percentage range between 0 and 100.")
+       }
+       
+       
+       
 
+      
         if (inputValue >= 40) {
             eligibleMsg.style.display = "flex";
             notEligibleMsg.style.display = "none";
